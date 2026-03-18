ef59f67f942d0a050bb12e2c593de0bc52318f4f
Vicki0111 <vicvignesh0111@gmail.com>
2026-03-18 14:41:08 +0530

    UC4:Room Search & Availability Check

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -13,50 +13,94 @@ class RoomInventory {
         inventory.put(roomType, count);
     }
 
-    public int getAvailability(String roomType) {
-        return inventory.getOrDefault(roomType, 0);
+    public Map<String, Integer> getRoomAvailability() {
+        return new HashMap<>(inventory);
     }
+}
 
-    public void updateAvailability(String roomType, int change) {
-        int current = inventory.getOrDefault(roomType, 0);
-        int updated = current + change;
+public class RoomSearchService {
 
-        if (updated < 0) {
-            System.out.println("Error: Not enough rooms available for " + roomType);
-        } else {
-            inventory.put(roomType, updated);
+    public void searchAvailableRooms(
+            RoomInventory inventory,
+            Room singleRoom,
+            Room doubleRoom,
+            Room suiteRoom) {
+
+        Map<String, Integer> availability = inventory.getRoomAvailability();
+
+        if (availability.getOrDefault("Single", 0) > 0) {
+            System.out.println("Single Room:");
+            singleRoom.displayRoomDetails();
+            System.out.println("Available Rooms: " + availability.get("Single") + "\n");
         }
-    }
 
-    public void displayInventory() {
-        System.out.println("\nCurrent Room Inventory:");
-        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
+        if (availability.getOrDefault("Double", 0) > 0) {
+            System.out.println("Double Room:");
+            doubleRoom.displayRoomDetails();
+            System.out.println("Available Rooms: " + availability.get("Double") + "\n");
+        }
+
+        if (availability.getOrDefault("Suite", 0) > 0) {
+            System.out.println("Suite Room:");
+            suiteRoom.displayRoomDetails();
+            System.out.println("Available Rooms: " + availability.get("Suite") + "\n");
         }
     }
 }
 
-public class BookMyStayApp {
+abstract class Room {
+
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: ₹" + pricePerNight);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
+public class UseCase4RoomSearch {
 
     public static void main(String[] args) {
 
         RoomInventory inventory = new RoomInventory();
+        inventory.addRoomType("Single", 5);
+        inventory.addRoomType("Double", 3);
+        inventory.addRoomType("Suite", 2);
 
-        inventory.addRoomType("Single Room", 10);
-        inventory.addRoomType("Double Room", 5);
-        inventory.addRoomType("Deluxe Room", 3);
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
 
-        inventory.displayInventory();
+        RoomSearchService searchService = new RoomSearchService();
 
-        System.out.println("\nAvailable Single Rooms: " +
-                inventory.getAvailability("Single Room"));
-
-        System.out.println("\nBooking 2 Single Rooms...");
-        inventory.updateAvailability("Single Room", -2);
-
-        System.out.println("Adding 1 Deluxe Room...");
-        inventory.updateAvailability("Deluxe Room", 1);
-
-        inventory.displayInventory();
+        System.out.println("Hotel Room Inventory Status:\n");
+        searchService.searchAvailableRooms(inventory, singleRoom, doubleRoom, suiteRoom);
     }
 }
